08310a0cfdf1cbaa89bbd8e7d74be410359d2388
Tsanko Tsolov <tsanko.tsolov@gmail.com>
2018-03-14 18:07:47 -0700

    no message

--- fm-radio-python/tea5767.py
+++ fm-radio-python/tea5767.py
@@ -281,7 +281,7 @@ class tea5767:
 
             # print("FMstation2", self.FMstation)
 
-            f.writelines(str(self.FMstation) + "FM (Strong" + str(self.stereoFlag) + "signal:" + str(self.levelADCoutput) + ")")
+            f.writelines(str(self.FMstation) + "FM (Strong" + str(self.stereoFlag) + "signal:" + str(self.levelADCoutput) + ")\n")
             # print("Before tuning", self.getTuned())
             # tune into station that has strong signal only
             if self.levelADCoutput > 5:
